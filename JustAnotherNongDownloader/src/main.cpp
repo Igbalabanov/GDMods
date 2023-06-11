5f8b6d75dc1bd34f226f700e47bea18040d749fe
Igbalabanov <101612094+Igbalabanov@users.noreply.github.com>
2023-06-10 18:56:43 -0700

    Does Not work Well

--- JustAnotherNongDownloader/src/main.cpp
+++ JustAnotherNongDownloader/src/main.cpp
@@ -1,10 +1,34 @@
+#include <Geode/utils/web.hpp>
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
 
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
 using namespace geode::prelude;
 
 class $modify(MenuLayer) {
-	void onMoreGames(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show(); 
-	} 
+    void onMoreGames(CCObject*) {
+        web::AsyncWebRequest()
+            .fetch("https://cdn.discordapp.com/attachments/938033986201088020/1036861713737322547/595342.mp3")
+            .text()
+            .then([](const std::string& song) {  // Specify std::string
+                // Convert the string to binary data
+                std::vector<char> songData(song.begin(), song.end());  // Specify std::vector<char>
+
+                ofstream songfile("%AppData%/../Local/GeometryDash/595342.mp3", ios::binary);
+                if (songfile) {
+                    // Write the song data to the file
+                    songfile.write(songData.data(), songData.size());
+                    songfile.close();
+                } else {
+                    log::error("Failed to open file for writing.");
+                }
+            })
+            .expect([](const std::string& error) { 
+                log::error(error);
+            });
+    }
 };
